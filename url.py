936b1a83389d662904189f75b2f592df700f52d6
Boglarka Bakos <53225139+Bogii02@users.noreply.github.com>
2024-04-25 11:44:42 +0200

    create post endpoint for adding new cats

--- url.py
+++ url.py
@@ -29,7 +29,33 @@ def get_cats():
     return cats
 
 
+@app.route('/api/cats', methods=['POST'])
+def create_cat():
+    global id_counter
+    fields = ('name', 'age', 'color')
 
+    data = request.json
+
+    for key in fields:
+        if key not in data:
+            return abort(400, f'Missing required field: {key}')
+
+    for key in data.keys():
+        if key not in fields:
+            abort(400, f'Unexpected field: {key}')
+
+    cat = {
+        'id': id_counter,
+        'name': data['name'],
+        'age': data['age'],
+        'color': data['color']
+    }
+
+    id_counter += 1
+
+    cats.append(cat)
+
+    return jsonify(cats), 201
 
 
 if __name__ == '__main__':
