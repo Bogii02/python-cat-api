00b3f0236637f189b4fbfb84c05554d5c0261c3e
Boglarka Bakos <53225139+Bogii02@users.noreply.github.com>
2024-05-03 15:48:03 +0200

    implement create database function and create cat function

--- data_manager.py
+++ data_manager.py
@@ -2,10 +2,31 @@ import database
 
 
 @database.connection_handler
+def create_db_table(cursor):
+    cursor.execute("""
+                    CREATE TABLE IF NOT EXISTS cats
+                        (id SERIAL PRIMARY KEY, name VARCHAR(50), age INTEGER, color VARCHAR(50));
+                    """)
+
+
+@database.connection_handler
+def create_cat(cursor, json_of_cat):
+
+    cursor.execute("""
+                    INSERT INTO cats (name, age, color)
+                    VALUES (%(name)s, %(age)s, %(color)s)
+                    RETURNING *;
+                    """,
+                   json_of_cat)
+    return_value = cursor.fetchall()
+    return return_value
+
+
+@database.connection_handler
 def get_all_cats(cursor):
     cursor.execute("""
                     SELECT *
                     FROM cats
                     """)
     return_value = cursor.fetchall()
-    return return_value
+    return {'cats': return_value}
